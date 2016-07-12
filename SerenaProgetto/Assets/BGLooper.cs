86e06963040ee440d7432e3f3d0c594aed48296b
andrea d'ambrosio <dambrosio.andry@gmail.com>
2016-07-12 16:01:19 +0200

    aggiunto bg loop

---
+++ SerenaProgetto/Assets/BGLooper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+
+    int numBGPanels = 6;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Triggered: " + collider.name);
+        float widthOfBGObject = ((BoxCollider2D)collider).size.x;
+        Vector3 pos = collider.transform.position;
+        pos.x += widthOfBGObject * numBGPanels; 
+        collider.transform.position = pos;
+    }
+}
