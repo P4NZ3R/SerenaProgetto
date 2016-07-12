b19ffb3a7411c7b2fd82ce487cf28377565aed11
andrea d'ambrosio <dambrosio.andry@gmail.com>
2016-07-12 12:13:47 +0200

    aggiunto movimento bird

---
+++ SerenaProgetto/Assets/BirdMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+    Vector3 velocity = Vector3.zero;
+    public Vector3 gravity;
+    public Vector3 flapVelocity;
+    public float maxSpeed = 5f;
+
+    bool didFlap = false;
+
+	void Start () {
+	
+	}
+	
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)   ||  Input.GetMouseButtonDown(0) )
+        {
+            didFlap = true;
+        }
+    }
+    
+
+
+	void FixedUpdate () 
+    {
+        velocity += gravity * Time.deltaTime;
+        if (didFlap == true)
+        {
+            didFlap = false;
+            velocity += flapVelocity;  
+        }
+        transform.position += velocity * Time.deltaTime;
+
+        
+    }
+
+}
