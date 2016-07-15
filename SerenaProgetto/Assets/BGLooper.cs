308b9a1d005660450f7e80cb2e3684206b866567
andrea d'ambrosio <dambrosio.andry@gmail.com>
2016-07-15 10:42:08 +0200

    aggiunto cod al ground

--- SerenaProgetto/Assets/BGLooper.cs
+++ SerenaProgetto/Assets/BGLooper.cs
@@ -10,7 +10,7 @@ public class BGLooper : MonoBehaviour {
         Debug.Log("Triggered: " + collider.name);
         float widthOfBGObject = ((BoxCollider2D)collider).size.x;
         Vector3 pos = collider.transform.position;
-        pos.x += widthOfBGObject * numBGPanels; 
+        pos.x += widthOfBGObject * numBGPanels - widthOfBGObject/2f; 
         collider.transform.position = pos;
     }
 }
